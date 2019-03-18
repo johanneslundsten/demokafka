b1d71fee2eb0576bead930bf684a1942fcb468c6
Johannes Lundsten <johannes.lundsten@bisnode.com>
2019-03-18 19:36:29 +0100

    mor stuff

--- src/main/java/com/bisnode/demo/Individual.java
+++ src/main/java/com/bisnode/demo/Individual.java
@@ -1,8 +1,77 @@
 package com.bisnode.demo;
 
+import java.util.Random;
+import java.util.UUID;
+
 /**
  * Created by johlun
  * on 2019-03-18.
  */
 public class Individual {
+
+    private static Random random = new Random();
+    private static String[] countries = {"SE", "DK", "NO"};
+    private static String[] genders = {"M", "F"};
+
+    private String gedi;
+    private int age;
+    private String gender;
+    private String countryCode;
+
+
+
+    public static Individual buildARandom(){
+        Individual individual = new Individual();
+        individual.setAge(random.nextInt(100));
+        individual.setCountryCode(countries[random.nextInt(countries.length)]);
+        individual.setGender(genders[random.nextInt(genders.length)]);
+        individual.setGedi(UUID.randomUUID().toString());
+        return individual;
+    }
+
+    public String getGedi() {
+        return gedi;
+    }
+
+    public Individual setGedi(String gedi) {
+        this.gedi = gedi;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Individual setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Individual setGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public Individual setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Individual{" +
+                "gedi='" + gedi + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/bisnode/demo/IndividualSerializer.java
+++ src/main/java/com/bisnode/demo/IndividualSerializer.java
@@ -1,8 +1,31 @@
 package com.bisnode.demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Created by johlun
  * on 2019-03-18.
  */
-public class IndividualSerializer {
+public class IndividualSerializer implements Serializer<Individual> {
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {}
+
+    @Override
+    public byte[] serialize(String topic, Individual data) {
+        try {
+            return new ObjectMapper().writeValueAsBytes(data);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
 }
